b2d2c88384986b863e395fa409421f53d13eee14
Pheobhe <l.defalco@mjus.gba.gob.ar>
2023-08-11 12:01:07 -0300

    Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Bienvenido a Vue.js App"/>
+  <Cabecera></Cabecera>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat sapiente qui sed soluta id. Veniam sint dolorem ut! Quidem, eaque sit magnam veniam aspernatur eos eius nisi natus tempore distinctio.</p>
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Cabecera
   }
 }
+
 </script>
+
+
+
 
 <style>
 #app {
@@ -20,7 +30,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #e2a82c;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Cabecera.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <a href="#">Pagina de inicio</a>
+        <a href="#">Menu de opciones</a>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Cabecera'
+}
+</script>
